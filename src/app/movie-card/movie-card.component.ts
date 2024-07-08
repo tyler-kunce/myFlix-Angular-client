fb4a97ea6e0e1e732a5065bd0cda7ad07ea37097
tyler-kunce <tyler.kunce@gmail.com>
2024-07-07 20:55:06 -0500

    Update movie-card.component.ts

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,8 +1,9 @@
 // src/app/movie-card/movie-card.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-card',
@@ -10,16 +11,26 @@ import { Router } from '@angular/router';
   styleUrl: './movie-card.component.scss'
 })
 export class MovieCardComponent {
+  @Input()
+  isFromFav: boolean = false;
   movies: any[] = [];
+  genre: any = '';
+  director: any = '';
+  user: any = {};
+  favoriteMovies: any[] = [];
+
+
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
+    public snackBar: MatSnackBar,
     private router: Router
   ) {
   }
 
   ngOnInit(): void {
     this.getMovies();
+    this.getFavorites();
   }
 
   getMovies(): void {
@@ -27,5 +38,13 @@ export class MovieCardComponent {
       this.movies = result;
       return this.movies;
     });
+  }
+
+  getFavorites(): void {
+    let user = localStorage.getItem('user');
+    if (user) {
+      let parsedUser = JSON.parse(user);
+      this.favoriteMovies = parsedUser.favoriteMovies
+    }
   }
 }
